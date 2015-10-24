256ff59e034b5b7d24a871594f7f6930cf40952b
Romain GAGNAIRE <romgagnaire@gmail.com>
2015-10-25 02:15:42 +0800

    - [CHANGE] Fixed package name in all related files

--- app/src/main/java/ca/uqac/myfirstgame/GamePanel.java
+++ app/src/main/java/ca/uqac/myfirstgame/GamePanel.java
@@ -1,10 +1,9 @@
-package com.miyou.myfirstgame;
+package ca.uqac.myfirstgame;
 
 import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.view.MotionEvent;
-import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
