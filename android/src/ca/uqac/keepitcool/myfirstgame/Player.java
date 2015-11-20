961b3c4caa3111d38c73520e1cb490ff8d967613
Romain GAGNAIRE <romgagnaire@gmail.com>
2015-11-20 16:51:26 -0500

    - [CHANGE] Refactor the app to fit into Android Standards

---
+++ android/src/ca/uqac/keepitcool/myfirstgame/Player.java
@@ -0,0 +1,42 @@
+package ca.uqac.keepitcool.myfirstgame;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+
+public class Player extends MovingObject{
+    private boolean isMoving=false;
+    private int dx,dy;
+    private Bitmap image;
+
+    public Player(int x, int y, int width,int height, int id, Bitmap res){
+        super(id, x, y, width, height);
+        image = res;
+    }
+
+    @Override
+    public void update(){
+        if(isMoving){
+            moveTowardPosition(dx,dy);
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas){
+        canvas.drawBitmap(image, getX(), getY(), null);
+    }
+
+    @Override
+    public void moveTowardPosition(int x, int y) {
+        super.moveTowardPosition(x, y);
+    }
+
+    public void setDxy(int xx, int yy) {
+        dx=xx;
+        dy=yy;
+    }
+
+    public void setMoving(boolean bb) {
+        isMoving = bb;
+    }
+}
