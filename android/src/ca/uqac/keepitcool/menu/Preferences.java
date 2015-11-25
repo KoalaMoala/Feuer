2d0a96bc47543fb999e92591612c45151b73713d
Romain GAGNAIRE <romgagnaire@gmail.com>
2015-11-25 18:18:08 -0500

    - [ADDITION] Added new class to save & load preferences

---
+++ android/src/ca/uqac/keepitcool/menu/Preferences.java
@@ -0,0 +1,57 @@
+package ca.uqac.keepitcool.menu;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import ca.uqac.keepitcool.quizz.scenario.Difficulty;
+
+public class Preferences {
+
+    private final static String difficultyKey = "difficulty";
+    private final static String soundKey = "sound";
+    private final static String nameKey = "name";
+
+    public static void updateDifficultySetting(Difficulty difficulty, Context context) {
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        prefs.edit().putString(difficultyKey, difficulty.name()).apply();
+    }
+
+    public static Difficulty getDifficultySetting(Context context) {
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String res = prefs.getString(difficultyKey, "MEDIUM");
+        return Difficulty.valueOf(res);
+    }
+
+    public static String getDifficultySettingAsString(Context context) {
+        Difficulty difficulty = getDifficultySetting(context);
+        switch (difficulty) {
+            case EASY:
+                return "Easy";
+            case HARD:
+                return "Hard";
+            default:
+                return "Medium";
+        }
+    }
+
+    public static void updateSoundSetting(boolean soundActivated, Context context) {
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        prefs.edit().putBoolean(soundKey, soundActivated).apply();
+    }
+
+    public static boolean getSoundSetting(Context context) {
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getBoolean(soundKey, true);
+    }
+
+    public static void updateNameSetting(String alias, Context context) {
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        prefs.edit().putString(nameKey, alias).apply();
+    }
+
+    public final static String getNameSetting(Context context) {
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getString(nameKey, "DefaultUsername");
+    }
+}
