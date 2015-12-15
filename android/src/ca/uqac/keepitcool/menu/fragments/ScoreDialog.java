6d98d03e800e1d302e7104036df6418a94b7e125
KoalaMoala <nicolas.amelot@outlook.com>
2015-12-15 11:14:31 -0500

    n'affiche plus les scores que s'ils existent

--- android/src/ca/uqac/keepitcool/menu/fragments/ScoreDialog.java
+++ android/src/ca/uqac/keepitcool/menu/fragments/ScoreDialog.java
@@ -1,21 +1,15 @@
 package ca.uqac.keepitcool.menu.fragments;
 
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.List;
 import java.util.TreeMap;
 
 import ca.uqac.keepitcool.R;
@@ -24,7 +18,8 @@ import ca.uqac.keepitcool.quizz.Preferences;
 
 public class ScoreDialog extends FragmentDialog {
 
-    TextView score_01, score_02, score_03, score_04,  score_05;
+    LinearLayout score_01, score_02, score_03, score_04, score_05;
+    TextView score_text_01, score_text_02, score_text_03, score_text_04,  score_text_05;
     HashMap<String, Float> scores;
     TreeMap<String, Float> scoresSorted;
 
@@ -33,11 +28,17 @@ public class ScoreDialog extends FragmentDialog {
         View view = inflater.inflate(R.layout.fragment_score, container, false);
 
         revealView = (CircularRevealView) view.findViewById(R.id.reveal);
-        score_01 = (TextView) view.findViewById(R.id.score_01_text);
-        score_02 = (TextView) view.findViewById(R.id.score_02_text);
-        score_03 = (TextView) view.findViewById(R.id.score_03_text);
-        score_04 = (TextView) view.findViewById(R.id.score_04_text);
-        score_05 = (TextView) view.findViewById(R.id.score_05_text);
+        score_text_01 = (TextView) view.findViewById(R.id.score_01_text);
+        score_text_02 = (TextView) view.findViewById(R.id.score_02_text);
+        score_text_03 = (TextView) view.findViewById(R.id.score_03_text);
+        score_text_04 = (TextView) view.findViewById(R.id.score_04_text);
+        score_text_05 = (TextView) view.findViewById(R.id.score_05_text);
+
+        score_01 = (LinearLayout) view.findViewById(R.id.score_01);
+        score_02 = (LinearLayout) view.findViewById(R.id.score_02);
+        score_03 = (LinearLayout) view.findViewById(R.id.score_03);
+        score_04 = (LinearLayout) view.findViewById(R.id.score_04);
+        score_05 = (LinearLayout) view.findViewById(R.id.score_05);
 
         frame = (FrameLayout) view.findViewById(R.id.frame);
         scores = new HashMap<>();
@@ -54,29 +55,51 @@ public class ScoreDialog extends FragmentDialog {
         if(!scores.isEmpty())
             scoresSorted.putAll(scores);
 
+        score_02.setVisibility(View.GONE);
+        score_03.setVisibility(View.GONE);
+        score_04.setVisibility(View.GONE);
+        score_05.setVisibility(View.GONE);
+
         //
         Object[] c = scores.keySet().toArray();
-        if(c.length > 0 ) score_01.setText(c[0] + " : " + scores.get(c[0]) + " s");
-        if(c.length > 1 ) score_02.setText(c[1] + " : " + scores.get(c[1]) + " s");
-        if(c.length > 2 ) score_03.setText(c[2] + " : " + scores.get(c[2]) + " s");
-        if(c.length > 3 ) score_04.setText(c[3] + " : " + scores.get(c[3]) + " s");
-        if(c.length > 4 ) score_05.setText(c[4] + " : " + scores.get(c[4]) + " s");
+        if(c.length > 0 )
+            score_text_01.setText(c[0] + " : " + scores.get(c[0]) + " s");
+        if(c.length > 1 )
+        {
+            score_text_02.setText(c[1] + " : " + scores.get(c[1]) + " s");
+            score_02.setVisibility(View.VISIBLE);
+        }
+        if(c.length > 2 )
+        {
+            score_text_03.setText(c[2] + " : " + scores.get(c[2]) + " s");
+            score_03.setVisibility(View.VISIBLE);
+        }
+        if(c.length > 3 )
+        {
+            score_text_04.setText(c[3] + " : " + scores.get(c[3]) + " s");
+            score_04.setVisibility(View.VISIBLE);
+        }
+        if(c.length > 4 )
+        {
+            score_text_05.setText(c[4] + " : " + scores.get(c[4]) + " s");
+            score_05.setVisibility(View.VISIBLE);
+        }
 
-        score_01.setOnClickListener(new View.OnClickListener() {
+        score_text_01.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO
             }
         });
 
-        score_02.setOnClickListener(new View.OnClickListener() {
+        score_text_02.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO
             }
         });
 
-        score_03.setOnClickListener(new View.OnClickListener() {
+        score_text_03.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO
